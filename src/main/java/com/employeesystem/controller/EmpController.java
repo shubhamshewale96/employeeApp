9b455b13ca84629edc3aa5156acb767c376a29b5
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-12-19 18:57:16 +0530

    edit data

--- src/main/java/com/employeesystem/controller/EmpController.java
+++ src/main/java/com/employeesystem/controller/EmpController.java
@@ -4,15 +4,22 @@ import com.employeesystem.entity.Employee;
 import com.employeesystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class EmpController {
-    @GetMapping( {"/", "/home"})
-    public String home(){
 
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping( {"/", "/home"})
+    public String home(Model m){
+        List<Employee> emp = employeeService.getAllEmp();
+        m.addAttribute("emp",emp);
         return "index";
     }
     @GetMapping(value={"/addemp", "/greeting"})
@@ -20,14 +27,26 @@ public class EmpController {
 
         return "addemp";
     }
-    @Autowired
-    EmployeeService employeeService;
-    @PostMapping("/register")
-    public String empRegister(@ModelAttribute Employee e){
-        //System.out.println(e);
-        employeeService.addEmp(e);
-        return "addemp";
-    }
 
+
+
+    @PostMapping("/register")
+    public String empRegister(@ModelAttribute Employee e, HttpSession session){
+        System.out.println(e);
+        employeeService.addEmp(e);
+        session.setAttribute("mess","Employee added successfully..");
+        return "redirect:/";
+    }
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id,Model m){
+        Employee e = employeeService.getEmpById(id);
+        m.addAttribute("emp",e);
+        return "edit";
+    }
+    @PostMapping("/update")
+    public String updateEmp(@ModelAttribute Employee e){
+        employeeService.addEmp(e);
+        return "redirect:/";
+    }
 
 }
--- src/main/java/com/employeesystem/service/EmployeeService.java
+++ src/main/java/com/employeesystem/service/EmployeeService.java
@@ -5,12 +5,29 @@ import com.employeesystem.repository.IEmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
     @Autowired
-    private IEmployeeRepo erepo;
+    private IEmployeeRepo eRepo;
+
     public void addEmp(Employee e){
 
-        erepo.save(e);
+        eRepo.save(e);
     }
+
+    public List<Employee> getAllEmp() {
+        return (List<Employee>) eRepo.findAll();
+    }
+    public Employee getEmpById(int id){
+      Optional<Employee> e = eRepo.findById(id);
+      if(e.isPresent()){
+          return e.get();
+        }
+        return null;
+    }
+
+
 }
